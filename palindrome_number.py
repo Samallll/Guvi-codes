52c2bc55bfa9d049391551ba2cf310404b26b0db
Samallll <43337563+Samallll@users.noreply.github.com>
2019-02-19 12:08:15 +0530

    Update palindrome_number.py

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,14 +1,11 @@
 n=input();
 k=n;
 sum=0;
-if(n<=1000):
-    while(n>0):
-        d=n%10;
-        sum=(sum*10)+d;
-        n=n//10;
-    if(k==sum):
-        print("yes");
-    else:
-        print("no");
+while(n>0):
+    d=n%10;
+    sum=(sum*10)+d;
+    n=n//10;
+if(k==sum):
+    print("yes");
 else:
-    print("value should not exceed 1000");
+    print("no");
